74f3224a39fa5bbc2117fc6825c11f6f329efe5c
igalvezr <igalvezr18@gmail.com>
2025-02-24 12:23:40 -0700

    Configuration class created

---
+++ src/main/java/com/igalvezr/todoapp/AppConfiguration.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.igalvezr.todoapp;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author ivan
+ */
+@Configuration
+@ComponentScan("com.igalvezr.todoapp")
+public class AppConfiguration {
+    
+}
