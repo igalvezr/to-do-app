050ad544b46189723d27744c2a6061bd50586022
igalvezr <igalvezr18@gmail.com>
2025-02-24 12:26:06 -0700

    State enum created

---
+++ src/main/java/com/igalvezr/todoapp/entities/State.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.igalvezr.todoapp.entities;
+
+/**
+ *
+ * @author ivan
+ */
+public enum State {
+    PENDING,
+    IN_PROCESS,
+    COMPLETED,
+    ABANDONED
+}
